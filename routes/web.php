1ee8eb40e3642c82dc69e3081ad36805d9feb307
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-12-27 22:12:15 +0545

    basic controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get("/about",function(){
     return view("about");
 });
+
+Route::get("/cars",[CarController::class,"index"]);
